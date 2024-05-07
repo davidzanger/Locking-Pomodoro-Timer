4710a8302dfe78734b99e95f2075e372602c7895
David Zanger <david.zanger@dlr.de>
2024-05-07 14:13:39 +0200

    Add docs.

--- src/pomo_info.rs
+++ src/pomo_info.rs
@@ -2,12 +2,27 @@ use std::time::Duration;
 
 use crate::pomodoro_options::PomodoroOptions;
 
+/// Represents the information related to a Pomodoro session.
 pub(crate) struct PomoInfo {
+    /// The number of Pomodoros remaining until a long break is triggered.
     pub(crate) pomodoros_till_long_break: i32,
+    /// Indicates whether a long break is approaching.
     pub(crate) is_long_break_coming: bool,
+    /// The duration of the next break.
     pub(crate) break_duration: Duration,
 }
+
 impl PomoInfo {
+    /// Creates a new `PomoInfo` instance from the given `PomodoroOptions` and counter.
+    ///
+    /// # Arguments
+    ///
+    /// * `options` - The `PomodoroOptions` struct containing the Pomodoro settings. Necessary to calculate the break duration and the number of Pomodoros until the next long break.
+    /// * `counter` - The current counter value indicating the number of completed Pomodoros.
+    ///
+    /// # Returns
+    ///
+    /// A new `PomoInfo` instance with the calculated values.
     pub(crate) fn from_options(options: &PomodoroOptions, counter: i32) -> Self {
         let pomodoros_till_long_break =
             options.interval_long_break - counter % options.interval_long_break;
--- src/pomodoro_options.rs
+++ src/pomodoro_options.rs
@@ -9,9 +9,9 @@ use std::io::Read;
 use std::path::PathBuf;
 use thiserror::Error;
 
+/// Struct representing the options for a Pomodoro timer.
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase", default = "PomodoroOptions::default")]
-/// Struct representing the options for a Pomodoro timer.
 pub struct PomodoroOptions {
     /// The duration of a single Pomodoro session in minutes.
     pub duration_pomodoro: i32,
@@ -32,6 +32,8 @@ pub struct PomodoroOptions {
     /// The end event to be executed after the additional Pomodoro after a Pomodoro session ends.
     pub end_event_additional_pomodoro: EndEvent,
 }
+
+/// Error type for verification errors of `PomodoroOptions`.
 #[derive(Error, Debug)]
 pub(crate) enum VerificationError {
     #[error("Pomodoro duration should be at least 1 minute.")]
@@ -47,6 +49,7 @@ pub(crate) enum VerificationError {
 }
 
 impl Default for PomodoroOptions {
+    /// Creates a new `PomodoroOptions` instance with default values.
     fn default() -> Self {
         PomodoroOptions {
             duration_pomodoro: 25,
@@ -65,6 +68,11 @@ impl Default for PomodoroOptions {
 }
 
 impl PomodoroOptions {
+    /// Verifies the validity of the `PomodoroOptions` instance.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `VerificationError` if any of the options are invalid.
     fn verify(&self) -> Result<(), VerificationError> {
         if self.duration_pomodoro < 1 {
             return Err(VerificationError::InvalidDuration);
@@ -93,60 +101,92 @@ impl PomodoroOptions {
     }
 }
 
+
+/// Error type for `PomodoroOptions` related errors.
 #[derive(Error, Debug)]
 pub(crate) enum PomodoroOptionsError {
     #[error("Failed to read options from JSON file at path: {:?}", _0)]
     OptionFileNotFound(PathBuf),
 }
 
-pub fn read_options_from_json(filepath_json: Option<PathBuf>) -> Result<PomodoroOptions> {
+/// Reads the `PomodoroOptions` from a JSON file.
+///
+/// If `filepath_json` is `Some`, it reads the options from the specified file.
+/// If `filepath_json` is `None`, it tries to find the options file next to the executable.
+///
+/// # Errors
+///
+/// Returns a `PomodoroOptionsError` if the options file is not found or if there are any other errors during the process.
+pub fn read_options_from_json(filepath_json: Option<PathBuf>) -> Result<PomodoroOptions> {    
     let file_path = match filepath_json {
-        Some(path) => path,
-        None => get_filepath_options_next_to_executable()?,
-    };
-    if !file_path.is_file() {
-        return Err(PomodoroOptionsError::OptionFileNotFound(file_path).into());
-    }
-    let mut file =
-        File::open(&file_path).with_context(|| format!("Failed to open file: {:?}", file_path))?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .with_context(|| format!("Failed to read file: {:?}", file_path))?;
+    Some(path) => path,
+    None => get_filepath_options_next_to_executable()?,
+};
+if !file_path.is_file() {
+    return Err(PomodoroOptionsError::OptionFileNotFound(file_path).into());
+}
+let mut file =
+    File::open(&file_path).with_context(|| format!("Failed to open file: {:?}", file_path))?;
+let mut contents = String::new();
+file.read_to_string(&mut contents)
+    .with_context(|| format!("Failed to read file: {:?}", file_path))?;
 
-    let mut data: PomodoroOptions = serde_json::from_str(&contents)
-        .with_context(|| format!("Failed to parse JSON file: {:?}", file_path))?;
-    match data.verify() {
-        Ok(_) => (),
-        Err(VerificationError::InvalidSoundFile) => {
-            println!("Sound file does not exist. Using default sound.");
-            if let EndEvent::Sound { filepath_sound } = &mut data.end_event_pomodoro {
-                *filepath_sound = PathBuf::new();
-            }
-            if let EndEvent::Sound { filepath_sound } = &mut data.end_event_additional_pomodoro {
-                *filepath_sound = PathBuf::new();
-            }
+let mut data: PomodoroOptions = serde_json::from_str(&contents)
+    .with_context(|| format!("Failed to parse JSON file: {:?}", file_path))?;
+match data.verify() {
+    Ok(_) => (),
+    Err(VerificationError::InvalidSoundFile) => {
+        println!("Sound file does not exist. Using default sound.");
+        if let EndEvent::Sound { filepath_sound } = &mut data.end_event_pomodoro {
+            *filepath_sound = PathBuf::new();
         }
-        Err(e) => return Err(e.into()),
+        if let EndEvent::Sound { filepath_sound } = &mut data.end_event_additional_pomodoro {
+            *filepath_sound = PathBuf::new();
+        }
     }
-    Ok(data)
+    Err(e) => return Err(e.into()),
+}
+Ok(data)
 }
 
+/// Writes the default `PomodoroOptions` to a JSON file next to the executable.
+///
+/// # Errors
+///
+/// Returns an error if there are any errors during the process of writing the options to the file.
 pub(crate) fn write_default_options_to_json_next_to_executable() -> Result<()> {
     let file_path = get_filepath_options_next_to_executable()?;
     let options = PomodoroOptions::default();
     write_options_to_json(&file_path, &options)
 }
 
+
+/// Writes the `PomodoroOptions` to a JSON file.
+///
+/// # Arguments
+///
+/// * `file_path` - The path to the JSON file.
+/// * `options` - The `PomodoroOptions` to write to the file.
+///
+/// # Errors
+///
+/// Returns an error if there are any errors during the process of writing the options to the file.
 pub(crate) fn write_options_to_json(
     file_path: &PathBuf,
     options: &PomodoroOptions) -> Result<()> {
-    let file = File::create(&file_path)
+        let file = File::create(&file_path)
         .with_context(|| format!("Failed to create file: {:?}", file_path))?;
     serde_json::to_writer_pretty(file, options)
         .with_context(|| format!("Failed to write to file: {:?}", file_path))?;
     Ok(())
 }
 
+
+/// Gets the path to the options file next to the executable.
+///
+/// # Errors
+///
+/// Returns an error if there are any errors during the process of getting the file path.
 fn get_filepath_options_next_to_executable() -> Result<PathBuf> {
     let filename = "pomodoro_options.json";
     let mut path = get_folderpath_executable()?;
@@ -154,6 +194,11 @@ fn get_filepath_options_next_to_executable() -> Result<PathBuf> {
     Ok(path)
 }
 
+/// Gets the folder path of the executable.
+///
+/// # Errors
+///
+/// Returns an error if there are any errors during the process of getting the folder path.
 fn get_folderpath_executable() -> Result<PathBuf> {
     let exe_path = env::current_exe().context("Failed to get executable path.")?;
     let mut file_path = exe_path.clone();
@@ -164,6 +209,7 @@ fn get_folderpath_executable() -> Result<PathBuf> {
 
 #[test]
 fn test_read_options_from_json() {
+    // Test case for `read_options_from_json` function.
     let filepath_test_json = get_project_root()
         .unwrap()
         .join("tests")
